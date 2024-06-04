61c76eb0402e2179525a4737d7604e56a6dbc3ab
semdev <tpal071997@gmail.com>
2024-06-04 17:07:18 +0900

    init commit

---
+++ src/main/java/com/sparta/lunchrecommender/LunchrecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.lunchrecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchrecommenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunchrecommenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/lunchrecommender/LunchrecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.lunchrecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchrecommenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
