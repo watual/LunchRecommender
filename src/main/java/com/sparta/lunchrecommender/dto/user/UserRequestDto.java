b47f1bde9f86642b3d49d3f1bfe7370297b87958
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-06-05 22:27:42 +0900

    [BUG] 로그인 & [FEATURE] 유효성 검사, 성공 및 실패 응답 추가
    [ bugfix ]
    update build.gradle
      - 기존 jjwt-root 의존성 제거 및 jjwt-api, jjwt-impl, jjwt-jackson 의존성 추가
    
    update user/LoginRequestDto
      - username -> loginId
    
    update security/JwtAuthenticationFilter
      - username -> loginId
    
    [ update ]
    update controller/UserController
    update dto/user/UserRequestDto
      - 회원가입 데이터 유효성 검증 기능 추가
    
    [ update ]
    update dto/HttpResponseDto
      - 상태코드명에서 코드번호로 교체
    
    update security/JwtAuthenticationFilter
      - HttpServletResponse 응답 추가
    
    [ del ]
    del security/Test
      - 임시 테스트 파일 삭제

--- src/main/java/com/sparta/lunchrecommender/dto/user/UserRequestDto.java
+++ src/main/java/com/sparta/lunchrecommender/dto/user/UserRequestDto.java
@@ -2,16 +2,24 @@ package com.sparta.lunchrecommender.dto.user;
 
 import com.sparta.lunchrecommender.constant.UserStatus;
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class UserRequestDto {
+    @Pattern(regexp = "^[A-Za-z\\d]{10,20}$")
     private String loginId;
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*?.,;:])[A-Za-z\\d!@#$%^&*?.,;:]{10,}$")
     private String password;
+    @NotNull
     private String name;
+    @NotNull
     private String nickname;
+    @Email
     private String email;
     private String intro;
 }
