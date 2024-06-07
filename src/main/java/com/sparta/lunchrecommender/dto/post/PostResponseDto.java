334e1a43425ae90069869575c4ab397677781bb1
yugi8 <yugi82828@naver.com>
2024-06-07 15:09:35 +0900

    update : 게시물 수정 토큰 검사
    update controller/PostController
    update dto/PostResponseDto
    update service/PostService

--- src/main/java/com/sparta/lunchrecommender/dto/post/PostResponseDto.java
+++ src/main/java/com/sparta/lunchrecommender/dto/post/PostResponseDto.java
@@ -1,6 +1,7 @@
 package com.sparta.lunchrecommender.dto.post;
 
 import com.sparta.lunchrecommender.entity.Post;
+import com.sparta.lunchrecommender.entity.User;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -8,17 +9,17 @@ import java.time.LocalDateTime;
 @Getter
 public class PostResponseDto {
 
-    private Long post_id;
-    private String login_id;
+    private Long postid;
+    private String loginid;
     private String nickname;
     private String content;
     private LocalDateTime created_at;
     private LocalDateTime modified_at;
 
-    public PostResponseDto(Post post) {
-        this.post_id = post.getPostId();
-//        this.login_id = post.getLoginId();
-//        this.nickname = post.getNickname();
+    public PostResponseDto(Post post, User user) {
+        this.postid = post.getPostId();
+        this.loginid = user.getLoginId();
+        this.nickname = user.getNickname();
         this.content = post.getContent();
         this.created_at = post.getCreatedAt();
         this.modified_at = post.getModifiedAt();
