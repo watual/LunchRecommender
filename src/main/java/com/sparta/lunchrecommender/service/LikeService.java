aa9e581535b607e853bcf51be23445d6b01180b5
semdev <tpal071997@gmail.com>
2024-06-07 21:10:04 +0900

    feat : 좋아요 취소기능
    add - service/LikeService : 중복 좋아요 막기, 취소 기능 구현

--- src/main/java/com/sparta/lunchrecommender/service/LikeService.java
+++ src/main/java/com/sparta/lunchrecommender/service/LikeService.java
@@ -27,63 +27,74 @@ public class LikeService {
 
         boolean isPost = likeRequestDto.getContentsType().equals(ContentsTypeEnum.POST);
 
-        if(isPost) { //게시글
+        if (isPost) { //게시글
             Post post = postRepository.findById(likeRequestDto.getContentId()).orElseThrow(
                     () -> new NullPointerException("존재하지 않는 게시글입니다.")
             );
             //좋아요를 이미 한 게시글인지?
-            if(likeRepository.findByUserIdAndContentsTypeAndContentId
-                    (userDetails.getUser().getUserId(),ContentsTypeEnum.POST, likeRequestDto.getContentId()) != null){
+            if (likeRepository.findByUserIdAndContentsTypeAndContentId
+                    (userDetails.getUser().getUserId(), ContentsTypeEnum.POST, likeRequestDto.getContentId()) != null) {
                 throw new NullPointerException("이미 좋아요를 누르셨습니다.");
             }
 
             Like like = new Like(likeRequestDto);
             like.setUserId(userDetails.getUser().getUserId());
             likeRepository.save(like);
-            post.setLikeCount(post.getLike_count()+1);
+            post.setLikeCount(post.getLike_count() + 1);
         } else { //댓글
             Comment comment = commentRepository.findById(likeRequestDto.getContentId()).orElseThrow(
                     () -> new NullPointerException("존재하지 않는 댓글입니다.")
             );
 
             //좋아요를 이미 한 댓글인지?
-            if(likeRepository.findByUserIdAndContentsTypeAndContentId
-                    (userDetails.getUser().getUserId(),ContentsTypeEnum.COMMENT, likeRequestDto.getContentId()) != null){
+            if (likeRepository.findByUserIdAndContentsTypeAndContentId
+                    (userDetails.getUser().getUserId(), ContentsTypeEnum.COMMENT, likeRequestDto.getContentId()) != null) {
                 throw new NullPointerException("이미 좋아요를 누르셨습니다.");
             }
 
             Like like = new Like(likeRequestDto);
             like.setUserId(userDetails.getUser().getUserId());
             likeRepository.save(like);
-            comment.setLikeCount(comment.getLike_count()+1);
+            comment.setLikeCount(comment.getLike_count() + 1);
         }
 
         return new ResponseEntity<>(new HttpResponseDto(HttpStatus.OK, "좋아요성공"), HttpStatus.OK);
     }
 
     public ResponseEntity<HttpResponseDto> likeCancel(LikeRequestDto likeRequestDto, UserDetailsImpl userDetails) {
-        Like like =  likeRepository.findByUserIdAndContentsTypeAndContentId(userDetails.getUser().getUserId(),ContentsTypeEnum.COMMENT, likeRequestDto.getContentId());
-        if(like == null){
-            throw new NullPointerException("데이터가 존재하지 않습니다.");
-        }
-        if(!like.getUserId().equals(userDetails.getUser().getUserId())) {
-            throw new IllegalArgumentException("유저 정보가 올바르지 않습니다.");
-        }
+
 
         boolean isPost = likeRequestDto.getContentsType().equals(ContentsTypeEnum.POST);
-        if(isPost){
+        if (isPost) {
             Post post = postRepository.findById(likeRequestDto.getContentId()).orElseThrow(
                     () -> new NullPointerException("존재하지 않는 게시글입니다.")
             );
-            post.setLikeCount(post.getLike_count()-1);
-        }
-        else{
+            Like like = likeRepository.findByUserIdAndContentsTypeAndContentId(userDetails.getUser().getUserId(), ContentsTypeEnum.POST, likeRequestDto.getContentId());
+            if (like == null) {
+                throw new NullPointerException("데이터가 존재하지 않습니다.");
+            }
+            if (!like.getUserId().equals(userDetails.getUser().getUserId())) {
+                throw new IllegalArgumentException("유저 정보가 올바르지 않습니다.");
+            }
+
+            post.setLikeCount(post.getLike_count() - 1);
+            likeRepository.delete(like);
+
+        } else {
             Comment comment = commentRepository.findById(likeRequestDto.getContentId()).orElseThrow(
                     () -> new NullPointerException("존재하지 않는 댓글입니다.")
             );
-            comment.setLikeCount(comment.getLike_count()-1);
+            Like like = likeRepository.findByUserIdAndContentsTypeAndContentId(userDetails.getUser().getUserId(), ContentsTypeEnum.COMMENT, likeRequestDto.getContentId());
+            if (like == null) {
+                throw new NullPointerException("데이터가 존재하지 않습니다.");
+            }
+            if (!like.getUserId().equals(userDetails.getUser().getUserId())) {
+                throw new IllegalArgumentException("유저 정보가 올바르지 않습니다.");
+            }
+
+            comment.setLikeCount(comment.getLike_count() - 1);
+            likeRepository.delete(like);
         }
-        likeRepository.delete(like);
 
 
         return new ResponseEntity<>(new HttpResponseDto(HttpStatus.OK, "좋아요취소"), HttpStatus.OK);
