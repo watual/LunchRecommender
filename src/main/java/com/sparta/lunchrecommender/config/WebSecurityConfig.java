66150f2f36f89d361cff953ddafd58feab69f1be
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-06-07 13:49:28 +0900

    update : 데이터 전체조회 허용
    [ update ]
    update config/WebSecurityConfig
      - /api/*/getList 와 같은 URL은 인가 생략
    update controller/PostController

--- src/main/java/com/sparta/lunchrecommender/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/lunchrecommender/config/WebSecurityConfig.java
@@ -66,6 +66,7 @@ public class WebSecurityConfig {
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                         .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
                         .requestMatchers("/api/user/signup").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers("/api/*/getList").permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
