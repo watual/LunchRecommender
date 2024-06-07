67b5d81e414f7458197d0ff2737a4bdbaed9272e
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-06-07 15:06:15 +0900

    update : getList 인가 허용, 유효성 검사 메세지 수정
    update config/WebSecurityConfig
      - api/ ~~ /getList 경로 요청 모두 허용
    
    update error/GlobalExceptionHandler
      - 유효성 검사 오류 메세지 변경

--- src/main/java/com/sparta/lunchrecommender/error/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/lunchrecommender/error/GlobalExceptionHandler.java
@@ -1,23 +1,55 @@
 package com.sparta.lunchrecommender.error;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RequiredArgsConstructor
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     // Handle All Exception
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+        log.error(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    // Valid 검증 오류
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        StringBuilder message = new StringBuilder("Validation failed for fields: ");
+        Map<String, String> errors = new HashMap<>();
+
+        // 유효성 검사에 실패한 필드 및 메시지 수집
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+
+            // 로그에 오류 메시지 기록
+            log.error("Validation failed for field: {} - {}", fieldName, errorMessage);
+
+            // 메시지에 필드 이름과 오류 메시지 추가
+            message.append(fieldName).append(" ");
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message.toString());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
+
 }
