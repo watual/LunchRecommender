ff213092b332bcb964221d2859561b97e0917154
M1ngD0ng <alswjd6807@naver.com>
2024-06-10 17:38:54 +0900

    feat : https 사용 구현
    [update LunchrecommenderApplication]
    https 설정 추가
    
    [update resources/application.yml]
    SSL 인증서 비밀번호 추가
    
    [add resources/bootsecurity.p12]
    SSL 인증서 파일 생성

--- src/main/java/com/sparta/lunchrecommender/LunchrecommenderApplication.java
+++ src/main/java/com/sparta/lunchrecommender/LunchrecommenderApplication.java
@@ -1,8 +1,15 @@
 package com.sparta.lunchrecommender;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -12,5 +19,30 @@ public class LunchrecommenderApplication {
     public static void main(String[] args) {
         SpringApplication.run(LunchrecommenderApplication.class, args);
     }
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
 
+        return tomcat;
+    }
+
+    private Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
 }
