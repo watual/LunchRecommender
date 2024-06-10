ad71184a6f4d0e238408c74f4d8f9131ccf33c48
semdev <tpal071997@gmail.com>
2024-06-10 15:43:09 +0900

    feat : 팔로우한 계정 게시글 조회
    add/update - controller/FollowController : 함수명 오탈자 수정, 팔로우 게시글 조회 기능 추가
    add - repository/FollowRepository : 조회 기능 추가
    add - repository/PostRepository : 조회 기능 추가
    add - service/FollowService : 팔로우 게시글 조회 기능 추가

--- src/main/java/com/sparta/lunchrecommender/controller/FollowController.java
+++ src/main/java/com/sparta/lunchrecommender/controller/FollowController.java
@@ -1,12 +1,16 @@
 package com.sparta.lunchrecommender.controller;
 
 import com.sparta.lunchrecommender.dto.HttpResponseDto;
+import com.sparta.lunchrecommender.dto.post.PostResponseDto;
+import com.sparta.lunchrecommender.entity.Post;
 import com.sparta.lunchrecommender.security.UserDetailsImpl;
 import com.sparta.lunchrecommender.service.FollowService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,15 +20,20 @@ public class FollowController {
     private final FollowService followService;
 
     @PostMapping("/follow/{userId}")
-    public ResponseEntity<HttpResponseDto> likeCreate(@PathVariable Long userId,
+    public ResponseEntity<HttpResponseDto> follow(@PathVariable Long userId,
                                                       @AuthenticationPrincipal UserDetailsImpl user){
         return followService.follow(userId,user);
     }
 
     @DeleteMapping("/unfollow/{userId}")
-    public ResponseEntity<HttpResponseDto> likeCancel(@PathVariable Long userId,
+    public ResponseEntity<HttpResponseDto> unfollow(@PathVariable Long userId,
                                                       @AuthenticationPrincipal UserDetailsImpl user){
         return followService.unfollow(userId,user);
+    }
+
+    @GetMapping("/follow/post/")
+    public List<Post> getFollowPost(@AuthenticationPrincipal UserDetailsImpl user){
+        return followService.getFollowPost(user);
     }
 
 
