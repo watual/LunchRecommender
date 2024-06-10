83f67227549b763167d13b2c956bc92664a04fe9
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-06-10 12:00:12 +0900

    update : 메일 인증 기능 추가
    [ feat : 메일 인증 기능 구현 ]
    update build.gradle
      - 메일 발송 라이브러리 의존성 추가
    
    update application.yml
      - 메일 인증 사용 설정
    
    add entity/EmailVerificationToken
      - 메일 인증 Entity 구현
    
    update controller/AuthController
      - 메일 인증 Controller 구현
    
    add repository/VerificationTokenRepository
      - 메일 인증 Repository 구현
    
    update service/UserService
      - 메일 인증 Service 구현 > 회원가입시 인증 메일 발송 + 메일 확인 service
    
    update constant/UserStatus
      - enum 메일 인증 전 상태 추가
    
    controller/UserController
      - 회원가입시 메일 인증요청 문구 추가
    
    update service/UserService
      - 로그인 실패 오류 서버 log에도 기록
    
    add error/exception/AuthorizationFailedException
      - 로그인 실패 오류 exception 커스텀 생성
    
    update util/UserUtil
      - 커스텀 생성 exception 사용, 로그인시 메일 인증 우선 요청 exception 추가

--- src/main/java/com/sparta/lunchrecommender/controller/AuthController.java
+++ src/main/java/com/sparta/lunchrecommender/controller/AuthController.java
@@ -2,7 +2,9 @@ package com.sparta.lunchrecommender.controller;
 
 import com.sparta.lunchrecommender.dto.HttpResponseDto;
 import com.sparta.lunchrecommender.dto.user.UserRequestDto;
+import com.sparta.lunchrecommender.entity.User;
 import com.sparta.lunchrecommender.jwt.JwtUtil;
+import com.sparta.lunchrecommender.repository.VerificationTokenRepository;
 import com.sparta.lunchrecommender.service.AuthService;
 import com.sparta.lunchrecommender.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,10 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -25,11 +24,19 @@ import java.io.IOException;
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
+    private final UserService userService;
+    private final VerificationTokenRepository verificationTokenRepository;
 
     @PostMapping("/refresh")
     public ResponseEntity<HttpResponseDto> refresh(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
         log.info("토큰 재발급 요청");
         authService.tokenRegeneration(httpServletRequest, httpServletResponse);
         return new ResponseEntity<>(new HttpResponseDto(HttpStatus.OK, "재발급 완료"), HttpStatus.OK);
+    }
+
+    @GetMapping("/confirm")
+    public ResponseEntity<HttpResponseDto> confirmToken(@RequestParam("token") String token) {
+        log.info("메일 인증");
+        return userService.confirmEmailToken(token);
     }
 }
